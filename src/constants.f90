736d13d1e5b09d88b4335524000055f33c6958f8
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2026-06-28 22:34:35 +0700

    feat(02-01): Extend constants_mod with C/F helpers and Rothfusz coeffs

--- src/constants.f90
+++ src/constants.f90
@@ -9,4 +9,31 @@ module constants_mod
     real(wp), parameter, public :: RH_MAX = 100.0_wp
     real(wp), parameter, public :: DEN_MIN = 0.0_wp
     real(wp), parameter, public :: DEN_MAX = 1.0_wp
+
+    real(wp), parameter, public :: ROTH_C1 = -42.379_wp
+    real(wp), parameter, public :: ROTH_C2 = 2.04901523_wp
+    real(wp), parameter, public :: ROTH_C3 = 10.14333127_wp
+    real(wp), parameter, public :: ROTH_C4 = -0.22475541_wp
+    real(wp), parameter, public :: ROTH_C5 = -0.00683783_wp
+    real(wp), parameter, public :: ROTH_C6 = -0.05481717_wp
+    real(wp), parameter, public :: ROTH_C7 = 0.00122874_wp
+    real(wp), parameter, public :: ROTH_C8 = 0.00085282_wp
+    real(wp), parameter, public :: ROTH_C9 = -0.00000199_wp
+
+    public :: c_to_f, f_to_c
+
+contains
+
+    elemental pure function c_to_f(c) result(f)
+        real(wp), intent(in) :: c
+        real(wp) :: f
+        f = c*9.0_wp/5.0_wp + 32.0_wp
+    end function c_to_f
+
+    elemental pure function f_to_c(f) result(c)
+        real(wp), intent(in) :: f
+        real(wp) :: c
+        c = (f - 32.0_wp)*5.0_wp/9.0_wp
+    end function f_to_c
+
 end module constants_mod
